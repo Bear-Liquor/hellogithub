79681c405a8dd6d26c121f13a16e8ac56a8a78df
马佳琪 <290263269@qq.com>
2015-04-04 23:30:20 +0800

    测试改

--- src/com/androd/HelloGitHub/MyActivity.java
+++ src/com/androd/HelloGitHub/MyActivity.java
@@ -13,5 +13,6 @@ public class MyActivity extends Activity {
 		setContentView(R.layout.main);
 		System.out.println("马上测试");
 		System.out.println("First Test");
+		System.out.println("Second Test");
 	}
 }
